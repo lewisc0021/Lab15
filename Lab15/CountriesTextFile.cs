54202365e6fb5d327ee64629f8d8f88353701ea2
Tommy Lewis <lewisc0021@gmail.com>
2016-11-30 14:36:29 -0500

    first commit with entire project

---
+++ Lab15/CountriesTextFile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class CountriesTextFile
+    {
+        //1.
+        public static List<string> Load()
+        {
+            List<string> CountryList = new List<string>();
+
+            string fileLocation = "../../countries.txt";
+
+            StreamReader reader = new StreamReader(fileLocation); 
+
+            string Data = reader.ReadToEnd().Trim();
+
+            string[] Records = Data.Split('\n');
+
+            foreach (string record in Records)
+            {
+                
+                CountryList.Add(record);
+            }
+            reader.Close();
+            return CountryList;
+        }
+
+        //2.
+        public static List<string> addCountry(string country,List<string>CountryList)
+        {
+            CountryList.Add(country);
+            return CountryList;
+        }
+
+        //3.
+        public static void displayCountries(List<string> CountryList)
+        {
+            foreach (string record in CountryList)
+            {
+                Console.WriteLine(record);
+            }
+        }
+
+        //4.
+        public static void Save(List<string> CountryList)
+        {
+            StreamWriter writer = new StreamWriter("../../countries.txt", true);
+            foreach (string record in CountryList)
+            {
+                writer.WriteLine(record);
+            }
+            writer.Close();
+        }
+
+    }
+}
---
+++ Lab15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myCountries = CountriesTextFile.Load();
+            Console.WriteLine("Welcome to Country Reader/Writer");
+            Console.WriteLine("Here are your options:");
+            Console.WriteLine("Enter 1 : Read Countries");
+            Console.WriteLine("Enter 2 : Write Countries");
+            Console.WriteLine("Enter 3 : Quit the application");
+            while (true)
+            {
+            Console.WriteLine("Please enter your choice");
+            int userChoice = int.Parse(Console.ReadLine());
+            string countryName = "";
+            if (userChoice == 1)
+            {
+                Console.WriteLine("Let's read");
+                CountriesTextFile.displayCountries(myCountries);
+
+            }
+
+            else if (userChoice == 2)
+            {
+                Console.WriteLine("Let's write");
+                Console.WriteLine("Please enter a country.");
+                countryName = Console.ReadLine().Trim().ToLower();
+                CountriesTextFile.addCountry(countryName, myCountries);
+                }
+
+            else if (userChoice == 3)
+             {
+                Console.WriteLine("Goodbye");
+                CountriesTextFile.Save(myCountries);
+                break;
+             }
+
+            }
+        }//MAIN
+    }//CLASS
+}//N.S.
